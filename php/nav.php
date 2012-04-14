bfd6d1e21dbb67ba8fc562193f66f758c056bf4b
Zachary Zimbler <zachzimbler@gmail.com>
2012-04-14 00:43:11 -0400

    nav created

--- php/nav.php
+++ php/nav.php
@@ -1,37 +1,14 @@
 <?
-/*
-<header id="nav-bar">
-	<div id="Logo"></div>
-	<?php if ($user) { ?>
-		
-	<?php } else { ?>
-	  <fb:login-button></fb:login-button>
-	<?php } ?>
-	<div id="fb-root"></div>
-	<script>
-	  window.fbAsyncInit = function() {
-		FB.init({
-		  appId: '<?php echo $facebook->getAppID() ?>',
-		  cookie: true,
-		  xfbml: true,
-		  oauth: true
-		});
-
-		FB.Event.subscribe('auth.login', function(response) {
-		  
-		  window.location.reload();
-		});
-		FB.Event.subscribe('auth.logout', function(response) {
-		  window.location.reload();
-		});
-	  };
-	  (function() {
-		var e = document.createElement('script'); e.async = true;
-		e.src = document.location.protocol +
-		  '//connect.facebook.net/en_US/all.js';
-		document.getElementById('fb-root').appendChild(e);
-	  }());
-	</script>
-</header>
- */
+	<div class="navbar navbar-fixed-top">
+		<div class="navbar-inner">
+			<div class="container">
+				<a class="brand" href="#">PSU Privacy</a>
+				<div class="nav-collapse">
+				<ul class="nav">
+				<li class="active"><a href="http://simply.io">From Simply.io</a></li>
+				</ul>
+				</div><!--/.nav-collapse -->
+			</div>
+		</div>
+	</div>
 ?>
