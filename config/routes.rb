a3b1af08145b6a68d80e6efbd88a7d34a7598342
David <dzz0615@gmail.com>
2012-04-13 22:05:26 -0400

    controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Bitlet::Application.routes.draw do
+  get "main/download"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +50,7 @@ Bitlet::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'main#index'
 
   # See how all your routes lay out with "rake routes"
 
