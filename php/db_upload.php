d5245ae79c11c364040eb3a831d1a1b8ce77d086
Team Simply <team@simply.io>
2012-04-14 03:40:01 -0700

    now supports multiple users

--- php/db_upload.php
+++ php/db_upload.php
@@ -2,24 +2,24 @@
 
 	include 'db_settings.php';  //load the DB Settings.
 
-	function GetUID($db, $email, $fbid)
+	function GetUID($email, $fbid)
 	{
 		//Query the user database and see if the user already has an account
-		$uid = mysql_query("SELECT id FROM user WHERE email = '$email'", $db);
-		if (!$uid) {
+		$result = mysql_query("SELECT id FROM user WHERE email = '$email'") or die();
+		if (mysql_num_rows($result) == 0) {
 			//create new user
 			mysql_query("INSERT INTO user (email, fbid, credits)
-				VALUES ('$email', '$fbid', '0')", $db);
-			$uid = mysql_query("SELECT id FROM user WHERE email = '$email'", $db);
+				VALUES ('$email', '$fbid', '0')") or die();;
+			$result = mysql_query("SELECT id FROM user WHERE email = '$email'") or die();
 		}
-		return $uid;
+		return mysql_result($result, 0);
 	}
 
-	function NewFile($db, $uid, $filename)
+	function NewFile($uid, $filename)
 	{
 		// Insert file into files table
 		mysql_query("INSERT INTO file (uid, name)
-			VALUES('$uid', '$filename')", $db);
+			VALUES('$uid', '$filename')") or die();
 	}
 
 ?>
