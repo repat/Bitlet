bcdc3c885f4f9266b4f451c6f3f76462dca3d73c
Zachary Zimbler <zachzimbler@gmail.com>
2012-04-14 09:49:22 -0400

    a bunch of changes

--- view/hero.php
+++ view/hero.php
@@ -1,4 +1,4 @@
-<div class="tagline-image">
+<div id="tagline-image">
 </div>
 <div class="container tagline">
 	<div class="row tagline-descr">
@@ -25,11 +25,11 @@
 
 <div class="container">
 	<div class="row">
-		<div class="span2 hero-circle" id="circle-1" onclick="javascript shit"></div>
-		<div class="span2 hero-circle" id="circle-2" onclick="javascript shit"></div>
-		<div class="span2 hero-circle" id="circle-3" onclick="javascript shit"></div>
-		<div class="span2 hero-circle" id="circle-4" onclick="javascript shit"></div>
-		<div class="span2 hero-circle" id="circle-5" onclick="javascript shit"></div>
-		<div class="span2 hero-circle" id="circle-6" onclick="javascript shit"></div>
+		<div class="span2 hero-circle" id="circle-1" "></div>
+		<div class="span2 hero-circle" id="circle-2" "></div>
+		<div class="span2 hero-circle" id="circle-3" "></div>
+		<div class="span2 hero-circle" id="circle-4" "></div>
+		<div class="span2 hero-circle" id="circle-5" "></div>
+		<div class="span2 hero-circle" id="circle-6" "></div>
 	</div>	
 </div>
