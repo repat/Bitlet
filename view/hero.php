b32e237639dcb3fbe4e274507602a355a266869c
Zachary Zimbler <zachzimbler@gmail.com>
2012-04-14 14:38:32 -0400

    oh yea

--- view/hero.php
+++ view/hero.php
@@ -32,22 +32,22 @@
 <div class="container">
 	<div class="row">
 		<div class="span2 hero-circle" id="circle-1">
-			<img src="/img/mainpage-icons/music.png" alt="Music" width="50px"/>
+			<img src="/img/mainpage-icons/music.png" class="hero-circle-padding" alt="Music" width="50px"/>
 		</div>
 		<div class="span2 hero-circle" id="circle-2">
-			<img src="/img/mainpage-icons/art.png" alt="Art" width="50px"/>
+			<img src="/img/mainpage-icons/art.png" class="hero-circle-padding" alt="Art" width="50px"/>
 		</div>
 		<div class="span2 hero-circle" id="circle-3">
-			<img src="/img/mainpage-icons/video.png" alt="Videos" width="50px"/>
+			<img src="/img/mainpage-icons/video.png" class="hero-circle-padding" alt="Videos" width="50px"/>
 		</div>
 		<div class="span2 hero-circle" id="circle-4">
-			<img src="/img/mainpage-icons/file.png" alt="Files" width="50px"/>
+			<img src="/img/mainpage-icons/file.png" class="hero-circle-padding" alt="Files" width="50px"/>
 		</div>
 		<div class="span2 hero-circle" id="circle-5">
-			<img src="/img/mainpage-icons/software.png" alt="Software" width="50px"/>
+			<img src="/img/mainpage-icons/software.png" class="hero-circle-padding" alt="Software" width="50px"/>
 		</div>
 		<div class="span2 hero-circle" id="circle-6">
-			<img src="img/mainpage-icons/photograph.png" alt="Photography" width="50px"/>
+			<img src="img/mainpage-icons/photograph.png" class="hero-circle-padding" alt="Photography" width="50px"/>
 		</div>
 	</div>	
 </div>
