83a8d4b5aefb19e619d7421fe192c7d6b16ff6f5
Zachary Zimbler <zachzimbler@gmail.com>
2012-04-14 10:54:20 -0400

    fuck js

--- js/hero-slider.js
+++ js/hero-slider.js
@@ -1,24 +1,5 @@
-$('#circle-1').click(function()
-{
-	$('#tagline-image').css("background", "url(img/bg.jpg)");
-});
-$('#circle-2').click(function()
-{
-	$('#tagline-image').css("background", "url(img/sch.jpg)");
-});
-$('#circle-3').click(function()
-{
-	$('#tagline-image').css("background", "url(img/bg.jpg)");
-});
-$('#circle-4').click(function()
-{
-	$('#tageline-image').css("background", "url(img/sch.jpg)");
-});
-$('#circle-5').click(function()
-{
-	$('#tagline-image').css("background", "url(img/bg.jpg)");
-});
-$('#circle-6').click(function()
-{
-	$('#tagline-image').css("background", "url(img/sch.jpg)");
+$(function() {
+	$('#circle-1').click(function() {
+			$('#tagline-image').attr('background', 'url() white'); 
+	});
 });
