606b92e180790fe2eb7d549b3070e05e8d067617
Zachary Zimbler <zachzimbler@gmail.com>
2012-04-14 10:33:11 -0400

    slider work

--- js/hero-slider.js
+++ js/hero-slider.js
@@ -1,24 +1,24 @@
 $('#circle-1').click(function()
 {
-	$('#tagline-image').css("background-image", "url(img/bg.jpg)");
+	$('#tagline-image').css("background", "url(img/bg.jpg)");
 });
 $('#circle-2').click(function()
 {
-	$('#tagline-image').css("background-image", "url(img/bg.jpg)");
+	$('#tagline-image').css("background", "url(img/sch.jpg)");
 });
 $('#circle-3').click(function()
 {
-	$('#tagline-image').css("background-image", "url(img/bg.jpg)");
+	$('#tagline-image').css("background", "url(img/bg.jpg)");
 });
 $('#circle-4').click(function()
 {
-	$('#tageline-image').css("background-image", "url(img/bg.jpg)");
+	$('#tageline-image').css("background", "url(img/sch.jpg)");
 });
 $('#circle-5').click(function()
 {
-	$('#tagline-image').css("background-image", "url(img/bg.jpg)");
+	$('#tagline-image').css("background", "url(img/bg.jpg)");
 });
 $('#circle-6').click(function()
 {
-	$('#tagline-image').css("background-image", "url(img/bg.jpg)");
+	$('#tagline-image').css("background", "url(img/sch.jpg)");
 });
