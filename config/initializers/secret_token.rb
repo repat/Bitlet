aa15a77354e9d8ba58e322c289beaa5c50ff411f
David <dzz0615@gmail.com>
2012-04-13 20:55:51 -0400

    first\!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bitlet::Application.config.secret_token = '1339e018094bb8cf31ec254d80e71b311ed753cea48dd94addd0e727ed0317e37dc1fef711a95ed3b0e48cd655f40435f285b9635c8edc9a5ab389d44f50b210'
