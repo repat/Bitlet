521850ba0cb8fec3cbad523f4c360bc665d40720
Zachary Zimbler <zachzimbler@gmail.com>
2012-04-14 06:48:48 -0400

    blabber text in hero

--- view/hero.php
+++ view/hero.php
@@ -5,14 +5,19 @@
 	<div class="row tagline-descr">
 		<div class="span9">
 			<div class="quote-background">
-				<div class="container mid">
+
+				<h1>Simply share & sell your digital content with the world</h1>
+
+				<h3>The easiest way to share your stuff online and make a profit. Join the revolution.</h3>
+				
+				<div class="container">
 					<h3>PSU user ID:</h3>
 					<form class="well form-inline">
 						<input type="text" class="input-small" placeholder="Email">
 						<input type="password" class="input-small" placeholder="Password">
 						<label class="checkbox">
 						<input type="checkbox"> Remember me </label>
-						<button type="submit" class="btn">Sign in</button>
+						<button type="submit" class="">Sign in</button>
 					</form>
 				</div>
 			</div>
