e59ca083f67ca7e1bcdae1e1678f748de1b0af52
Zachary Zimbler <zachzimbler@gmail.com>
2012-04-14 14:46:31 -0400

    in theory the image siwtching should work

--- js/hero-slider.js
+++ js/hero-slider.js
@@ -1,3 +1,24 @@
-function changeBg(bg){
-	document.getElementById("tagline-image").style.background = bg;
-}
+$('#circle-1').click(function()
+{
+	$('#tagline-image').css("background", "url(/img/music.jpg)");
+});
+$('#circle-2').click(function()
+{
+	$('#tagline-image').css("background", "url(/img/music.jpg)");
+});
+$('#circle-3').click(function()
+{
+	$('#tagline-image').css("background", "url(/img/music.jpg)");
+});
+$('#circle-4').click(function()
+{
+	$('#tagline-image').css("background", "url(/img/music.jpg)");
+});
+$('#circle-5').click(function()
+{
+	$('#tagline-image').css("background", "url(/img/music.jpg)");
+});
+$('#circle-6').click(function()
+{
+	$('#tagline-image').css("background", "url(/img/music.jpg)");
+});
