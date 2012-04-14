f635c6c23400812523ae68064d52f64b689cf672
Zachary Zimbler <zachzimbler@gmail.com>
2012-04-14 00:33:13 -0400

    header changes

--- php/header.php
+++ php/header.php
@@ -25,6 +25,9 @@
 ?>
 
 <head>
+	<!--[if lt IE 9]>
+	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->    
 	<link href="/css/style.css" rel="stylesheet" type="text/css">
 	<link href="/css/bootstrap.css" rel="stylesheet" type="text/css">
 </head>
