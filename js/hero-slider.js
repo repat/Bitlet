b4955302a93aa32b56afa4cfae8d292c6c27941e
Zachary Zimbler <zachzimbler@gmail.com>
2012-04-14 14:49:50 -0400

    adshfkajdshf

--- js/hero-slider.js
+++ js/hero-slider.js
@@ -4,21 +4,21 @@ $('#circle-1').click(function()
 });
 $('#circle-2').click(function()
 {
-	$('#tagline-image').css("background", "url(/img/music.jpg)");
+	$('#tagline-image').css("background", "url(/img/digiart.jpg)");
 });
 $('#circle-3').click(function()
 {
-	$('#tagline-image').css("background", "url(/img/music.jpg)");
+	$('#tagline-image').css("background", "url(/img/movies.jpg)");
 });
 $('#circle-4').click(function()
 {
-	$('#tagline-image').css("background", "url(/img/music.jpg)");
+	$('#tagline-image').css("background", "url(/img/digfile.jpg)");
 });
 $('#circle-5').click(function()
 {
-	$('#tagline-image').css("background", "url(/img/music.jpg)");
+	$('#tagline-image').css("background", "url(/img/Files.jpg)");
 });
 $('#circle-6').click(function()
 {
-	$('#tagline-image').css("background", "url(/img/music.jpg)");
+	$('#tagline-image').css("background", "url(/img/photos.jpg)");
 });
