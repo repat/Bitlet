9794ec05bd4cdfa4356541c09d166848a1611edc
Zachary Zimbler <zachzimbler@gmail.com>
2012-04-14 14:23:01 -0400

    icons added

--- view/hero.php
+++ view/hero.php
@@ -31,11 +31,23 @@
 
 <div class="container">
 	<div class="row">
-		<div class="span2 hero-circle" id="circle-1"></div>
-		<div class="span2 hero-circle" id="circle-2"></div>
-		<div class="span2 hero-circle" id="circle-3"></div>
-		<div class="span2 hero-circle" id="circle-4"></div>
-		<div class="span2 hero-circle" id="circle-5"></div>
-		<div class="span2 hero-circle" id="circle-6"></div>
+		<div class="span2 hero-circle" id="circle-1">
+			<img src="/img/mainpage-icons/music.png" alt="Music" width="80px"/>
+		</div>
+		<div class="span2 hero-circle" id="circle-2">
+			<img src="/img/mainpage-icons/art.png" alt="Art" width="80px"/>
+		</div>
+		<div class="span2 hero-circle" id="circle-3">
+			<img src="/img/mainpage-icons/video.png" alt="Videos" width="80px"/>
+		</div>
+		<div class="span2 hero-circle" id="circle-4">
+			<img src="/img/mainpage-icons/file.png" alt="Files" width="80px"/>
+		</div>
+		<div class="span2 hero-circle" id="circle-5">
+			<img src="/img/mainpage-icons/software.png" alt="Software" width="80px"/>
+		</div>
+		<div class="span2 hero-circle" id="circle-6">
+			<img src="img/mainpage-icons/photograph.png" alt="Photography" width="80px"/>
+		</div>
 	</div>	
 </div>
