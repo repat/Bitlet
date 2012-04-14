02437aac1d3c78d536369d12c84590610e65c1de
Team Simply <team@simply.io>
2012-04-13 22:35:52 -0700

    include bootstrap

--- view/header.php
+++ view/header.php
@@ -31,5 +31,6 @@
 	<link href="/css/style.css" rel="stylesheet" type="text/css">
 	<link href="/css/bootstrap.css" rel="stylesheet" type="text/css">
 	<link href="/css/loading.css" rel="stylesheet" type="text/css">
+	<script type="text/javascript" src="/js/bootstrap.js"></script>
 </head>
 
