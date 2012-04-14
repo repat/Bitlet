751ad702aa64698aa3fcd9672c945e99f2b59773
Zachary Zimbler <zachzimbler@gmail.com>
2012-04-14 01:02:32 -0400

    nav updated, removed link to simply

--- view/nav.php
+++ view/nav.php
@@ -1,12 +1,7 @@
-	<div class="navbar navbar-fixed-top">
-		<div class="navbar-inner">
-			<div class="container">
-				<a class="brand" href="#">PSU Privacy</a>
-				<div class="nav-collapse">
-				<ul class="nav">
-				<li class="active"><a href="http://simply.io">From Simply.io</a></li>
-				</ul>
-				</div><!--/.nav-collapse -->
-			</div>
+<div class="navbar navbar-fixed-top">
+	<div class="navbar-inner">
+		<div class="container">
+			<a class="brand" href="#">Bitlet</a>
 		</div>
 	</div>
+</div>
