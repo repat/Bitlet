cbd4d0c7ca98b062b7610ffeb77b2d385ac5a6d0
David <dzz0615@gmail.com>
2012-04-29 01:34:40 -0400

    updated hero

--- view/hero.php
+++ view/hero.php
@@ -1,54 +1,48 @@
-<div id="tagline-image">
-</div>
-<div class="container tagline">
-	<div class="row tagline-descr">
-		<div class="span6">
-			<div class="quote-background">
-				<h1>Share & Sell Your Digital Content With the World.</h1>
-				<h2 class="intro">The easiest way to share your stuff online and make a profit. Join the revolution.</h2>
-				<div>
-					<form class="form-inline" id="upload" action="upload.php" method="post" enctype="multipart/form-data">
-						<div class="input-prepend">
-					        <span class="add-on" id="emaila"><i id="email-icon" class="icon-envelope"></i></span>
-							<input class="input-medium" id="email" type="text" name="email" placeholder="Email" onkeypress="return event.keyCode!=13">
-						</div>
 
-						<button type="button" class="btn btn-large btn-primary">
-							<div class="file-div">
-							<input type="file" name="file" id="button" class="file-button"
-								 onchange="load(''); fade('loading'); upload.submit()"/>
-							</div>
-							Upload File
-						</button>
-					</form>
-					<div class="email-suggestion-wrap">
-						<div class="email-suggestion"></div>
-					</div>
-				</div>
-			</div>
+<!-- SLIDER CODE GOES HERE -->
+<div id="tagline-image">
+	<div id="headerButtons">
+		<div id="theButtons"></div>
+	</div>
+	<div id="myCarousel" class="carousel slide">
+		<!-- Carousel items -->
+		<div class="carousel-inner">
+			<div class="active item"> <img src="img/bg/photos.jpg"> </div>
+			<div class="item"> <img src="img/bg/art.jpg"></div>
+			<div class="item"> <img src="img/bg/doc.jpg"> </div>
+			<div class="item"> <img src="img/bg/movies.jpg"> </div>
+			<div class="item"> <img src="img/bg/music.jpg"> </div>
+			<div class="item"> <img src="img/bg/files.jpg"> </div>
 		</div>
 	</div>
 </div>
 
-<div class="container">
-	<div class="row">
-		<div class="span2 hero-circle" id="circle-1">
-			<img src="/img/mainpage-icons/music.png" class="hero-circle-padding" alt="Music" width="50px"/>
+
+<!-- TEXT BOX CODE GOES HERE -->
+<div class="tagline">
+	<div class="tagline-descr">
+		<div class="quote-background">
+			<h1>Share & Sell Your Digital Content With the World.</h1>
+			<h2 class="intro">The easiest way to share your stuff online and make a profit. Join the revolution.</h2>
+			<div>
+				<form class="form-inline" id="upload" action="upload.php" method="post" enctype="multipart/form-data">
+					<div class="input-prepend">
+						<span class="add-on" id="emaila"><i id="email-icon" class="icon-envelope"></i></span>
+						<input class="input-medium" id="email" type="text" name="email" placeholder="Email" onkeypress="return event.keyCode!=13">
+					</div>
+
+					<button type="button" class="btn btn-large btn-primary">
+						<div class="file-div">
+						<input type="file" name="file" id="button" class="file-button"
+							 onchange="load(''); fade('loading'); upload.submit()"/>
+						</div>
+						Upload File
+					</button>
+				</form>
+				<div class="email-suggestion-wrap">
+					<div class="email-suggestion"></div>
+				</div>
+			</div>
 		</div>
-		<div class="span2 hero-circle" id="circle-2">
-			<img src="/img/mainpage-icons/art.png" class="hero-circle-padding" alt="Art" width="50px"/>
-		</div>
-		<div class="span2 hero-circle" id="circle-3">
-			<img src="/img/mainpage-icons/video.png" class="hero-circle-padding" alt="Videos" width="50px"/>
-		</div>
-		<div class="span2 hero-circle" id="circle-4">
-			<img src="/img/mainpage-icons/file.png" class="hero-circle-padding" alt="Files" width="50px"/>
-		</div>
-		<div class="span2 hero-circle" id="circle-5">
-			<img src="/img/mainpage-icons/software.png" class="hero-circle-padding" alt="Software" width="50px"/>
-		</div>
-		<div class="span2 hero-circle" id="circle-6">
-			<img src="img/mainpage-icons/photograph.png" class="hero-circle-padding" alt="Photography" width="50px"/>
-		</div>
-	</div>	
+	</div>
 </div>
