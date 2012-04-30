11d2269df55e343b0a4b53e0659506f12a9295cf
Team Simply <team@simply.io>
2012-04-30 09:53:53 -0700

    fixed navbar padding issue

--- view/nav.php
+++ view/nav.php
@@ -1,9 +1,9 @@
 <div class="navbar navbar-fixed-top">
 	<div class="navbar-inner">
 		<div class="container">
-			<a href="http://bitlet.co"><img src="/img/logoSmall.png"></a>
+			<a class="brand" href="http://bitlet.co"><img src="/img/logoSmall.png"></a>
 
-			<div class="pull-right">
+			<div class="nav pull-right">
 				<a class="navbar-text" href="faq.php">FAQ</a>
 				<a class="navbar-text" href="http://simply.io">About</a>
 			</div>
