35b9da11fc368e9bc90db9e4a17b4f5fd5126313
David <dzz0615@gmail.com>
2012-04-30 12:32:55 -0400

    added navbar stuff

--- view/nav.php
+++ view/nav.php
@@ -3,5 +3,10 @@
 		<div class="container">
 			<a href="http://bitlet.co"><img src="/img/logoSmall.png"></a>
 		</div>
+
+		<div class="pull-right">
+			<a href="http://bitlet.co">FAQ</a>
+			<a href="http://bitlet.co">About</a>
+		</div>
 	</div>
 </div>
