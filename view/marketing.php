b7a576937ea804abb3e50bf8e6b7c6fbae7347a7
Arianna Simpson <ariannahsimpson@gmail.com>
2012-05-03 10:53:25 -0400

    How it works

--- view/marketing.php
+++ view/marketing.php
@@ -1,37 +1,37 @@
 <div class="container marketing">
 	<h1>How Does Bitlet Work?</h1>
+	<p>
+	Bitlet allows you to sell your digital media instantly. Now you can profit simply by uploading your photos, songs, videos, and more.
+	</p>
 	<br>
 
 	<div class="row">
 		<div class="span4">
-			<h2>Sell Instantly</h2>
-
+		<h2>Sell Instantly</h2>
 			<p>
-			All you have to do is fill in your email, and upload the file. That's it! We'll email you an admin
-			link for you to keep track of your purchases, and a share link to give to your customers to buy 
-			your product.
+			Just fill in your email, and upload the file. That's it! We'll email you two links: an admin link so you can keep track of your sales, and a share link to give to your customers.
 			</p>
 		</div>
 
 		<div class="span4">
-			<h2>No Bank Account Required</h2>
+			<h2>Share It</h2>
 		
 			<p>
-			Once you're ready to claim your profits, just press the "check out" button on the admin page, enter
-			your address, and we'll mail you a check. No need to enter in your bank account number. If you really
-			like the convenience of having profits instantly deposited into your bank account, we can do that too.
+				Once you have the link, you can Tweet it, post it on Facebook, Pin it, glue it to your forehead, whatever. 
+				Anyone with the link can buy your product. You can even put it in a QR code and have people buy it on their mobile phone.	
 			</p>
 		</div>
 
 		<div class="span4">
-			<h2>Share the Link With Anyone</h2>
+			<h2>Claim Your Cash</h2>
 
-			<p>
-			Tweet it, post it on facebook, pin it, whatever. Anyone with the link can buy your product. You can even
-			put it in a QR code and have people buy it on their mobile phone.
-			</p>
+				<p>
+				Once you're ready to collect your profits, just press the "Check Out" button on the admin page.
+			    Enter your address, and we'll mail you a check. No need to enter in your bank account number. If you really like the convenience of having profits instantly deposited into your bank account, we can do that too.
+				</p>
 		</div>
 	</div>
+	<h3>Yeah, it’s really that easy. But if you still have a question, email us! </h3>
 	<br>
 	<br>
 </div>
