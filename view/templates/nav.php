2881892684e8c4db507c3d298012111b3a9352b3
David <dzz0615@gmail.com>
2012-05-05 14:51:24 -0400

    htaccess should be good

--- view/templates/nav.php
+++ view/templates/nav.php
@@ -1,7 +1,7 @@
 <div class="navbar navbar-fixed-top">
 	<div class="navbar-inner">
 		<div class="container">
-			<a class="brand" href="http://bitlet.co"><img src="img/logoSmall.png"></a>
+			<a class="brand" href="http://bitlet.co"><img src="/img/logoSmall.png"></a>
 
 			<div class="nav pull-right">
 				<a class="navbar-text" onclick="showFAQ();"href="#">FAQ</a>
