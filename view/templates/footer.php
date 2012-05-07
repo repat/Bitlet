968d31480edf99850537d882db07c56a34681c0c
Zachary Zimbler <zachzimbler@gmail.com>
2012-05-07 02:17:23 -0400

    changing the bg

--- view/templates/footer.php
+++ view/templates/footer.php
@@ -7,6 +7,7 @@
 		<div style="text-align:center" class="container">
 			<a href="#" class="footer-text"> Terms</a>
 			<a href="#" class="footer-text"> Privacy </a>
+			<a href="#" class="footer-text"> Affiliates </a>
 			<a href="#" class="footer-text">Security</a></p>
 			<p class="copyright-text"> Copyright 2012, All Rights Reserved </p>
 			
