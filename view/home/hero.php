d814f67ed2e00c01394247ab04ed66994fa720ad
Arianna Simpson <ariannahsimpson@gmail.com>
2012-05-06 23:26:48 -0400

    changes

--- view/home/hero.php
+++ view/home/hero.php
@@ -13,10 +13,10 @@
 			<tr>
 				<th style="width:300px; vertical-align:text-top;"><h3> Just fill in your email, and upload a file you want to sell.</h3></th>
 				<th style="width:470px; vertical-align:text-top;"><h3>We give you a link you can Pin, Tweet, post to Facebook, or email out. Anyone with the link can buy your product. </h3></th>
-				<th style="width:300px; vertical-align:text-top;" ><h3>Once you're ready to collect your profits, just press the "Check Out" button. </h3></th>
+				<th style="width:300px; vertical-align:text-top;" ><h3>Once you're ready to collect your profits, simply click "check out". </h3></th>
 			</tr>
 			</table>
-			<hr class="index bar">
+			<hr class="index-bar">
 			
 				<form  style="text-align:left;" class="form-inline" target="upload_iframe" id="upload" action="/ajax/upload" method="post" enctype="multipart/form-data">
 					<div class="input-prepend">
@@ -52,12 +52,12 @@
 	<div id="myCarousel" class="carousel slide">
 		<!-- Carousel items -->
 		<div class="carousel-inner">
-			<div class="active item"> <img src="img/bg/photos.jpg"> </div>
-			<div class="item"> <img src="img/bg/art.jpg"></div>
+			<div class="active item"> <img class="carouselImage" src="img/bg/musician.jpg"> </div>
+			<!--<div class="item"> <img src="img/bg/art.jpg"></div>
 			<div class="item"> <img src="img/bg/doc.jpg"> </div>
 			<div class="item"> <img src="img/bg/movies.jpg"> </div>
 			<div class="item"> <img src="img/bg/music.jpg"> </div>
-			<div class="item"> <img src="img/bg/files.jpg"> </div>
+			<div class="item"> <img src="img/bg/files.jpg"> </div>-->
 		</div>
 	</div>
 </div>
