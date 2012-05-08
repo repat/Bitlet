822e5cbca69f2221426a33777113b7e385383e2e
Zachary Zimbler <zachzimbler@gmail.com>
2012-05-08 03:27:05 -0400

    changed footer links. The about/terms/privacy/security page needs content but is essentially done

--- view/templates/footer.php
+++ view/templates/footer.php
@@ -5,11 +5,11 @@
 		<br>
 		<br>
 		<div style="text-align:center" class="container">
-			<a href="#" class="footer-text">Terms</a>
-			<a href="#" class="footer-text">Privacy</a>
+			<a href="/about/#terms" class="footer-text">Terms</a>
+			<a href="/about/#privacy" class="footer-text">Privacy</a>
 			<a href="http://affiliates.bitlet.co/" class="footer-text">Affiliates</a>
-			<a href="#" class="footer-text">Help</a>
-			<a href="#" class="footer-text">Security</a>
+			<a href="#" class="footer-text" onclick="showFAQ();">Help</a>
+			<a href="/about/#security" class="footer-text">Security</a>
 			<p class="copyright-text"><br />&copy;2012 Bitlet, Inc</p>
 			
 		</div>
