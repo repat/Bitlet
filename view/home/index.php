e5e4979c3fa16a80179d46fda75432e46844e7c3
Zachary Zimbler <zachzimbler@gmail.com>
2012-05-12 00:34:58 -0400

    readujsted the shadow on feature box

--- view/home/index.php
+++ view/home/index.php
@@ -3,28 +3,30 @@
 		<div id="home-featured-left">
 		</div><!-- end of home-feature-left -->
 		<div id="home-featured-right">
-			<form  style="text-align:center;" class="form-inline" target="upload_iframe" id="upload" 
-				action="/ajax/upload" method="post" enctype="multipart/form-data">
-				<div class="input-prepend">
-					<span class="add-on" id="emaila"><i id="email-icon" class="icon-envelope"></i></span>
-					<input class="input-medium" id="email" type="text" name="email" placeholder="Email" onkeypress="return event.keyCode!=13">
-				</div><!-- end of input-prepend -->
+			<div id="content">
+				<form  style="text-align:center;" class="form-inline" target="upload_iframe" id="upload" 
+					action="/ajax/upload" method="post" enctype="multipart/form-data">
+					<div class="input-prepend">
+						<span class="add-on" id="emaila"><i id="email-icon" class="icon-envelope"></i></span>
+						<input class="input-medium" id="email" type="text" name="email" placeholder="Email" onkeypress="return event.keyCode!=13">
+					</div><!-- end of input-prepend -->
 
-				<button type="button" class="upload-button btn btn-large btn-primary" 
-					onclick="$('.file-button').click();">
-					Upload File
-				</button>
-				<input type="file" name="file" id="button" class="file-button" style="display:hidden"
-					 onchange="
-						upload.submit();
-						// disable the file selection button
-						$('.upload-button').attr('disabled', 'disabled'); 
-						$('.file-button').attr('disabled', 'disabled');"
-				/>
-			</form>
-			<div class="email-suggestion-wrap">
-				<div class="email-suggestion"></div>
-			</div>
+					<button type="button" class="upload-button btn btn-large btn-primary" 
+						onclick="$('.file-button').click();">
+						Upload File
+					</button>
+					<input type="file" name="file" id="button" class="file-button" style="display:hidden"
+						 onchange="
+							upload.submit();
+							// disable the file selection button
+							$('.upload-button').attr('disabled', 'disabled'); 
+							$('.file-button').attr('disabled', 'disabled');"
+					/>
+				</form>	
+				<div class="email-suggestion-wrap">
+					<div class="email-suggestion"></div>
+				</div>
+			</div><!-- content -->
 			<div id="top-triangle" class="triangle"></div> 
 			<div id="bottom-triangle" class="triangle"></div> 
 		</div><!-- end of home-feature-right -->
