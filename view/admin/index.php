20c55e2a0d9ae3b6af86b0be8bb934847433bc16
Arianna Simpson <ariannahsimpson@gmail.com>
2012-05-18 12:06:41 -0400

    admin table

--- view/admin/index.php
+++ view/admin/index.php
@@ -9,18 +9,26 @@
 		18 
 		</div>
 		<div class="sideDataText" id="salesText"> 
-		 Sales
+		 Total Sales
 		</div>
 			<div class="sideData" id="dollars">
 		500 
 		</div>
 		<div class="sideDataText" id="dollarsText"> 
-		 Dollars
+		 Total Dollars
 		</div>
 	</div>
-	<div class= "sidebar-div">
-		<div class="tablediv">
-			<table  class="table table-bordered tablebar" style="text-align:left" cellpadding"10px" > 
+		<div class= "sidebar-div">
+			<div class="tablediv">
+				<div class="tabbable"> 
+				  <ul class="nav nav-tabs">
+					<li class="active"><a href="#tab1" data-toggle="tab">Purchases</a></li>
+					<li><a href="#tab2" data-toggle="tab">Sales</a></li>
+				  </ul>
+				  <div class="tab-content">
+					<div class="tab-pane active" id="tab1">
+					  <p>Things You've Bought</p>
+					  <table  class="table table-bordered tablebar" style="text-align:left" cellpadding"10px" > 
 				<tr class="toprow">
 					<th>Image</th>
 					<th>Image Name</th> 
@@ -52,6 +60,45 @@
 					<td></td>
 				</tr> 
 			</table>
+					</div>
+					<div class="tab-pane" id="tab2">
+					  <p>Things You're Selling</p>
+					  <table  class="table table-bordered tablebar" style="text-align:left" cellpadding"10px" > 
+				<tr class="toprow">
+					<th>Image</th>
+					<th>Image Name</th> 
+					<th>Download</th>
+					<th>Share</th>
+				</tr>
+				<tr> 
+					<td>Lala</td> 
+					<td>lala</td>
+					<td>603.0303</td>
+					<td></td>
+				</tr> 
+				<tr> 
+					<td>Lala</td> 
+					<td>lala</td>
+					<td>603.0303</td>
+					<td></td>
+				</tr> 
+				<tr> 
+					<td>Lala</td> 
+					<td>lala</td>
+					<td>603.0303</td>
+					<td></td>
+				</tr> 
+				<tr> 
+					<td>Lala</td> 
+					<td>lala</td>
+					<td>603.0303</td>
+					<td></td>
+				</tr> 
+			</table>
+					</div>
+				  </div>
+				</div>
+			
 		</div>
 	</div>
 	<div class="rightSideDiv">
