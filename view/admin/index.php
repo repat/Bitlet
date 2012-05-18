4566846d924e4c64950fbc89e9f7ea28316adcea
David <dzz0615@gmail.com>
2012-05-18 10:14:11 -0400

    conflict fixed

--- view/admin/index.php
+++ view/admin/index.php
@@ -1,10 +1,11 @@
 <div class="main-shebang">
 	<div class="topDiv">
 	</div>
+
 	<div class="leftSideDiv">
 	Hello
 	</div>
-<<<<<<< HEAD
+
 	<div class= "sidebar-div">
 		<div class="tablediv">
 			<table  class="table table-bordered tablebar" style="text-align:left" cellpadding"10px" > 
@@ -40,44 +41,8 @@
 				</tr> 
 			</table>
 		</div>
-=======
-
-	<div class= "sidebar-div">
-		<div class="tablediv">
-		<table  class="table table-bordered tablebar" style="text-align:left" cellpadding"10px" > 
-			<tr class="toprow">
-				<th>Image</th>
-				<th>Image Name</th> 
-				<th>Download</th>
-				<th>Share</th>
-			</tr>
-			<tr> 
-				<td>Lala</td> 
-				<td>lala</td>
-				<td>603.0303</td>
-				<td></td>
-			</tr> 
-			<tr> 
-				<td>Lala</td> 
-				<td>lala</td>
-				<td>603.0303</td>
-				<td></td>
-			</tr> 
-				<tr> 
-				<td>Lala</td> 
-				<td>lala</td>
-				<td>603.0303</td>
-				<td></td>
-			</tr> 
-				<tr> 
-				<td>Lala</td> 
-				<td>lala</td>
-				<td>603.0303</td>
-				<td></td>
-			</tr> 
-		</table>
-	</div>
 
 		HIIIII
+	</div>
 </div>
 	
