8b660d720ec8f0acb970b0759731cf701c853c9f
Arianna Simpson <ariannahsimpson@gmail.com>
2012-05-18 10:03:14 -0400

    table

--- view/admin/index.php
+++ view/admin/index.php
@@ -4,41 +4,41 @@
 	<div class="leftSideDiv">
 	Hello
 	</div>
-<div class= "sidebar-div">
-	<div class="tablediv">
-<table  class="table table-bordered tablebar" style="text-align:left" cellpadding"10px" > 
-		<tr class="toprow">
-			<th>Image</th>
-			<th>Image Name</th> 
-			<th>Download</th>
-			<th>Share</th>
-		</tr>
-		<tr> 
-			<td>Lala</td> 
-			<td>lala</td>
-			<td>603.0303</td>
-			<td></td>
-		</tr> 
-		<tr> 
-			<td>Lala</td> 
-			<td>lala</td>
-			<td>603.0303</td>
-			<td></td>
-		</tr> 
-			<tr> 
-			<td>Lala</td> 
-			<td>lala</td>
-			<td>603.0303</td>
-			<td></td>
-		</tr> 
-			<tr> 
-			<td>Lala</td> 
-			<td>lala</td>
-			<td>603.0303</td>
-			<td></td>
-		</tr> 
-	</table>
-	</div>
+	<div class= "sidebar-div">
+		<div class="tablediv">
+			<table  class="table table-bordered tablebar" style="text-align:left" cellpadding"10px" > 
+				<tr class="toprow">
+					<th>Image</th>
+					<th>Image Name</th> 
+					<th>Download</th>
+					<th>Share</th>
+				</tr>
+				<tr> 
+					<td>Lala</td> 
+					<td>lala</td>
+					<td>603.0303</td>
+					<td></td>
+				</tr> 
+				<tr> 
+					<td>Lala</td> 
+					<td>lala</td>
+					<td>603.0303</td>
+					<td></td>
+				</tr> 
+					<tr> 
+					<td>Lala</td> 
+					<td>lala</td>
+					<td>603.0303</td>
+					<td></td>
+				</tr> 
+					<tr> 
+					<td>Lala</td> 
+					<td>lala</td>
+					<td>603.0303</td>
+					<td></td>
+				</tr> 
+			</table>
+		</div>
 		HIIIII
 	</div>
 	
