d756668405cdfd2eb02d66af31ceff689c780a98
Arianna Simpson <ariannahsimpson@gmail.com>
2012-05-18 20:34:57 -0400

    admin table

--- view/admin/index.php
+++ view/admin/index.php
@@ -1,109 +1,114 @@
 <div class="main-shebang">
-	<div class="topDiv">
-		<h1> Welcome, Arianna! </h1>    
-		<form class="form-search">
-  			<input type="text" class="input-medium search-query" placeholder="Search">
-		</form>
-	</div>
-	<div class="leftSideDiv">
-		<div class="sideData" id="number0fSales">
-		18 
+		<div class="topDiv">
+			<h1> Welcome, Arianna! </h1>    
+			<form class="form-search">
+				<input type="text" class="input-medium search-query" placeholder="Search">
+			</form>
 		</div>
-		<div class="sideDataText" id="salesText"> 
-		 Total Sales
+		<div class="leftSideDiv">
+			<div class="sideData" id="number0fSales">18</div>
+			<div class="sideDataText" id="salesText">Total Sales</div>
+				<div class="sideData" id="dollars">500</div>
+			<div class="sideDataText" id="dollarsText">Total Dollars</div>
 		</div>
-			<div class="sideData" id="dollars">
-		500 
-		</div>
-		<div class="sideDataText" id="dollarsText"> 
-		 Total Dollars
-		</div>
-	</div>
 		<div class= "sidebar-div">
 			<div class="tablediv">
-				<div class="tabbable"> 
-				  <ul class="nav nav-tabs">
-					<li class="active"><a href="#tab1" data-toggle="tab" id="tab">Purchases</a></li>
-					<li><a href="#tab2" data-toggle="tab">Sales</a></li>
-					<li><a href="#tab3" data-toggle="tab">Analytics</a></li>
-				  </ul>
-				  <div class="tab-content">
-					<div class="tab-pane active" id="tab1">
-					  <p style="padding-left:12px">Things You've Bought</p>
-					  <table  class="table table-bordered tablebar" style="text-align:left" cellpadding"10px" > 
-				<tr class="toprow">
-					<th>Image</th>
-					<th>Image Name</th> 
-					<th>Download</th>
-					<th>Share</th>
-				</tr>
-				<tr> 
-					<td>Lala</td> 
-					<td>lala</td>
-					<td>603.0303</td>
-					<td></td>
-				</tr> 
-				<tr> 
-					<td>Lala</td> 
-					<td>lala</td>
-					<td>603.0303</td>
-					<td></td>
-				</tr> 
-				<tr> 
-					<td>Lala</td> 
-					<td>lala</td>
-					<td>603.0303</td>
-					<td></td>
-				</tr> 
-				<tr> 
-					<td>Lala</td> 
-					<td>lala</td>
-					<td>603.0303</td>
-					<td></td>
-				</tr> 
-			</table>
-					</div>
-					<div class="tab-pane" id="tab2">
-					  <p style="padding-left:12px">Things You're Selling</p>
-					  <table  class="table table-bordered tablebar" style="text-align:left" cellpadding"10px" > 
-				<tr class="toprow">
-					<th>Image</th>
-					<th>Image Name</th> 
-					<th>Download</th>
-					<th>Share</th>
-				</tr>
-				<tr> 
-					<td>Lala</td> 
-					<td>lala</td>
-					<td>603.0303</td>
-					<td></td>
-				</tr> 
-				<tr> 
-					<td>Lala</td> 
-					<td>lala</td>
-					<td>603.0303</td>
-					<td></td>
-				</tr> 
-				<tr> 
-					<td>Lala</td> 
-					<td>lala</td>
-					<td>603.0303</td>
-					<td></td>
-				</tr> 
-				<tr> 
-					<td>Lala</td> 
-					<td>lala</td>
-					<td>603.0303</td>
-					<td></td>
-				</tr> 
-			</table>
-					</div>
-				  </div>
+					<div class="tabbable"> 
+					  <ul class="nav nav-tabs">
+						<li class="active"><a href="#tab1" data-toggle="tab" id="tab">Purchases</a></li>
+						<li><a href="#tab2" data-toggle="tab">Sales</a></li>
+						<li><a href="#tab3" data-toggle="tab">Analytics</a></li>
+					  </ul>
+						  <div class="tab-content">
+							<div class="tab-pane active" id="tab1">
+					<table  class="table table tablebar" style="text-align:left" cellpadding"10px" > 
+						<tr> 
+							<td>Lala</td> 
+							<td>lala</td>
+							<td> <button class="btn btn-small btn-primary">Download</button>
+							</td>
+							<td> 
+								www.cats.com
+								<img id="twitter" src="/img/twitter.png"/>
+								<img id="facebook" src="/img/facebook.png"/>
+							</td>
+						
+						</tr> 
+						<tr> 
+							<td>Lala</td> 
+							<td>lala</td>
+							<td> <button class="btn btn-small btn-primary">Download</button>
+							</td>
+							<td> 
+								<img id="twitter" src="/img/twitter.png"/>
+								<img id="facebook" src="/img/facebook.png"/>
+							</td>
+							
+						</tr> 
+						<tr> 
+							<td>Lala</td> 
+							<td>lala</td>
+							<td><button class="btn btn-small btn-primary">Download</button>
+							</td>
+							<td> 
+								<img id="twitter" src="/img/twitter.png"/>
+								<img id="facebook" src="/img/facebook.png"/>
+							</td>
+							
+						</tr> 
+						<tr> 
+							<td>Lala</td> 
+							<td>lala</td>
+							<td><button class="btn btn-small btn-primary">Download</button>
+							</td>
+							<td> 
+								<img id="twitter" src="/img/twitter.png"/>
+								<img id="facebook" src="/img/facebook.png"/>
+							</td>
+							
+						</tr> 
+					</table>
 				</div>
-			
+				<div class="tab-pane" id="tab2">
+					<table  class="table table tablebar" style="text-align:left" cellpadding"10px" > 
+		
+						<tr> 
+							<td>Lala</td> 
+							<td>lala</td>
+							<td>http://ilovecats.com</td>
+							<td> 
+								<img id="twitter" src="/img/twitter.png"/>
+								<img id="facebook" src="/img/facebook.png"/>
+							</td>
+						</tr> 
+						<tr> 
+							<td>Lala</td> 
+							<td>lala</td>
+							<td>http://ilovecats.com</td>
+							<td><img id="twitter" src="/img/twitter.png"/>
+								<img id="facebook" src="/img/facebook.png"/></td>
+						</tr> 
+						<tr> 
+							<td>Lala</td> 
+							<td>lala</td>
+							<td>http://ilovecats.com</td>
+							<td><img id="twitter" src="/img/twitter.png"/>
+								<img id="facebook" src="/img/facebook.png"/></td>
+						</tr> 
+						<tr> 
+							<td>Lala</td> 
+							<td>lala</td>
+							<td>http://ilovecats.com</td>
+							<td><img id="twitter" src="/img/twitter.png"/>
+								<img id="facebook" src="/img/facebook.png"/></td>
+						</tr> 
+					</table>
+				</div>
+			  </div>
+			</div>
 		</div>
 	</div>
-	<div class="rightSideDiv">
-	</div>
+	<div id="shadow"></div>
+	<div class="rightSideDiv"></div>
 </div>
 	
