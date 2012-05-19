e7498a1236058f31becf146ae43006d508bfed8a
David <dzz0615@gmail.com>
2012-05-18 20:27:07 -0400

    rearranged lib

--- lib/file.php
+++ lib/file.php
@@ -1,26 +1,5 @@
 <?
-
 	include 'db_settings.php';  //load the DB Settings.
-
-	// get the uid of user based on his email, also links up facebook id
-	function GetUID($email, $fbid)
-	{
-		// default password return as null if user already exist
-		$pass = null;
-
-		//Query the user database and see if the user already has an account
-		$result = mysql_query("SELECT id FROM user WHERE email = '$email'") or die();
-		if(mysql_num_rows($result) == 0) {
-			//create new user
-			$salt = substr(md5(rand()), 0, 8); 
-			$pass = substr(md5(rand()), 0, 8); 
-			$hashed_pass = sha1($pass.$salt);
-			mysql_query("INSERT INTO user (email, fbid, password, salt, credits)
-				VALUES ('$email', '$fbid', '$hashed_pass', '$salt', '0')") or die();
-			return mysql_insert_id();
-		}
-		return array(mysql_result($result, 0), $pass);
-	}
 
 	// check for valid email/pass, return true on successful authentication
 	// add a new file under input user uid
@@ -66,20 +45,6 @@
 		$uid = $row[1];
 		// add price to user id credits
 		mysql_query("UPDATE user SET credits = credits + '$price' WHERE id='$uid'") or die();
-	}
-
-	// show how much money a user has left
-	function GetCredits($uid)
-	{
-		$result = mysql_query("SELECT credits FROM user WHERE id='$uid'") or die();
-		return mysql_result($result, 0);
-	}
-
-	// get user from 
-	function GetUserInfo($uid)
-	{
-		$result = mysql_query("SELECT * FROM user WHERE id='$uid'") or die();
-		return mysql_fetch_assoc($result);
 	}
 
 	// returns an array of fid from user
---
+++ lib/user.php
@@ -0,0 +1,38 @@
+<?
+	include 'db_settings.php';  //load the DB Settings.
+
+	// get the uid of user based on his email, also links up facebook id
+	function GetUID($email, $fbid)
+	{
+		// default password return as null if user already exist
+		$pass = null;
+
+		//Query the user database and see if the user already has an account
+		$result = mysql_query("SELECT id FROM user WHERE email = '$email'") or die();
+		if(mysql_num_rows($result) == 0) {
+			//create new user
+			$salt = substr(md5(rand()), 0, 8); 
+			$pass = substr(md5(rand()), 0, 8); 
+			$hashed_pass = sha1($pass.$salt);
+			mysql_query("INSERT INTO user (email, fbid, password, salt, credits)
+				VALUES ('$email', '$fbid', '$hashed_pass', '$salt', '0')") or die();
+			return mysql_insert_id();
+		}
+		return array(mysql_result($result, 0), $pass);
+	}
+
+	// get user from 
+	function GetUserInfo($uid)
+	{
+		$result = mysql_query("SELECT * FROM user WHERE id='$uid'") or die();
+		return mysql_fetch_assoc($result);
+	}
+
+	// show how much money a user has left
+	function GetCredits($uid)
+	{
+		$result = mysql_query("SELECT credits FROM user WHERE id='$uid'") or die();
+		return mysql_result($result, 0);
+	}
+
+?>
