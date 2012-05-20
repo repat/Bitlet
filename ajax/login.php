74e77917f004977665c438db86b94ec4b824c912
David <dzz0615@gmail.com>
2012-05-20 00:26:15 -0400

    multiple uploads seemst to work when looged in

---
+++ ajax/login.php
@@ -0,0 +1,14 @@
+<?
+	$email = $_POST['email'];
+	$pass = $_POST['pass'];
+
+	$res = Authenticate($email, $pass);
+
+	if($res == false) {
+		echo json_encode(array("success"=>false));
+	} else {
+		// set this session as logged in
+		$_SESSION['uid'] = $res;
+		echo json_encode(array("success"=>true));
+	}
+?>
