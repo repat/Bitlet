d075d7a150c17363188bf9b7fa83fffdcebdcac5
David <dzz0615@gmail.com>
2012-05-23 21:36:24 -0400

    made sticky scroll relative

--- view/admin/scroll.js
+++ view/admin/scroll.js
@@ -1,10 +1,13 @@
+var $window = $(window),
+	$stickyEl = $('.rightSideDiv');
 
- var $window = $(window),
-       $stickyEl = $('.rightSideDiv');
- var elTop = $stickyEl.offset().top;
-       
-   $window.scroll(function() {
-        var windowTop = $window.scrollTop();
-
-        $stickyEl.toggleClass('sticky', windowTop > elTop);
-    });
+var elTop = $stickyEl.offset().top;
+   
+$window.scroll(function() {
+	var windowTop = $window.scrollTop();
+	
+	// if above window, move the element down
+	if(windowTop > elTop) {
+		$stickyEl.offset({top: windowTop});
+	}
+});
