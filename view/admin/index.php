a6d6bc627c427fc5025ba8ddd88c460801d25306
Arianna Simpson <ariannahsimpson@gmail.com>
2012-05-24 22:34:26 -0400

    changes to the left side div

--- view/admin/index.php
+++ view/admin/index.php
@@ -24,10 +24,14 @@ if($uid >= 0) {
 		</div>
 		<div class="leftSideDiv">
 			<div class="leftSideData sticky-div">
-				<div class="sideData" id="number0fSales">18</div>
-				<div class="sideDataText" id="salesText">Total Sales</div>
-					<div class="sideData" id="dollars">500</div>
-				<div class="sideDataText" id="dollarsText">Total Dollars</div>
+				<div class="sideData" id="credits">18</div>
+				<div class="sideDataText" id="totalCredits">Total Credits</div>
+				<div class="sideData" id="sales">50</div>
+				<div class="sideDataText" id="totalSales">Total Sales</div>
+				<div class="sideData" id="bought">12</div>
+				<div class="sideDataText" id="totalBought">Purchased</div>
+				<div class="sideData" id="selling">9</div>
+				<div class="sideDataText" id="totalSelling">Products</div>
 			</div>
 		</div>
 		<div class= "centerDiv">
