18a91c095923bd222a561ac2c34cfd7214d8d683
Arianna Simpson <ariannahsimpson@gmail.com>
2012-05-27 16:43:51 -0400

    dave destroys arianna's page

--- view/about/index.php
+++ view/about/index.php
@@ -1,12 +1,30 @@
-<div class="container well teamDiv">
-	<div class="namesDiv">
-		<h1 class="team" id="dave"><a href="http://twitter.com/#!/DavidZhangToGo">David Zhang </a></h1>
-		<h1 class="team" id="zach"><a href="http://twitter.com/#!/zzimbler">Zach Zimbler </a></h1>
-		<h1 class="team" id="ari"><a href="http://twitter.com/#!/AriannaSimpson">Arianna Simpson</a></h1>
+<div class="container well">
+	<div class="top">
+		<div class="cloud"><img src="img/drawings/bitcloud.png"/></div>
+		<div class="about">
+			<h3>Meet Bitlet </h3>
+			<p> David, Zach, and Arianna started Bitlet because they thought people should have an easy way to sell the cool things they create. 
+			David, Zach, and Arianna started Bitlet because they thought people should have an easy way to sell the cool things they create. 
+			David, Zach, and Arianna started Bitlet because they thought people should have an easy way to sell the cool things they create.  </p>
+		</div>
 	</div>
-	<div class="picsDiv">
-		<a href="http://twitter.com/#!/DavidZhangToGo"><img class="teamPics" id="dave" rel="tooltip" title="Dave is really weird. Sometimes he fixes Arianna's bugs." src="img/david.png"/></a>
-		<a href="http://twitter.com/#!/zzimbler"><img class="teamPics" id="zach" el="tooltip" title="Zach apparently likes Frisbee, and broken bones."src="/img/zach.png"></a>
-		<a href="http://twitter.com/#!/AriannaSimpson"><img class="teamPics" el="tooltip" title="Arianna is awesome, end of story."id="ari" src="/img/arianna.png"/></a>
+	<hr>
+	<div class="bottom">
+		<div class="namesDiv">
+			<h1 class="team" id="dave"><a href="http://twitter.com/#!/DavidZhangToGo">David Zhang </a></h1>
+			<h1 class="team" id="zach"><a href="http://twitter.com/#!/zzimbler">Zach Zimbler </a></h1>
+			<h1 class="team" id="ari"><a href="http://twitter.com/#!/AriannaSimpson">Arianna Simpson</a></h1>
+		</div>
+		<div class="picsDiv">
+			<a href="http://twitter.com/#!/DavidZhangToGo">
+				<img class="teamPics" id="dave" rel="tooltip" title="Dave is really weird. Sometimes he fixes Arianna's bugs." src="img/team/david.png"/>
+			</a>
+			<a href="http://twitter.com/#!/zzimbler">
+				<img class="teamPics" id="zach" rel="tooltip" title="Zach apparently likes Frisbee, and broken bones." src="/img/team/zach.png"/>
+			</a>
+			<a href="http://twitter.com/#!/AriannaSimpson">
+				<img class="teamPics" id="ari" rel="tooltip" title="Arianna is awesome, end of story." src="/img/team/arianna.png"/>
+			</a>
+		</div>
 	</div>
 </div>
