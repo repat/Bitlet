e58dd88b23aa507082622edd7731042759c0159b
Arianna Simpson <ariannahsimpson@gmail.com>
2012-05-26 23:17:37 -0400

    team changes

--- view/about/index.php
+++ view/about/index.php
@@ -3,10 +3,11 @@
 		<h1 class="team"><a href="http://twitter.com/#!/DavidZhangToGo">David Zhang </a></h1>
 		<h1 class="team"><a href="http://twitter.com/#!/zzimbler">Zach Zimbler </a></h1>
 		<h1 class="team"><a href="http://twitter.com/#!/AriannaSimpson">Arianna Simpson</a></h1>
+	</div>
 	<div class="picsDiv">
-		<img class="teamPics" src="/img/arianna.png"/>
 		<img class="teamPics" src="img/david.png"/>
 		<img class="teamPics" src="/img/zach.png">
+		<img class="teamPics" src="/img/arianna.png"/>
 	</div>
 </div>
 
