aaa8dd90fb29c07f56de7bafa65c9b85f88239e9
David <dzz0615@gmail.com>
2012-05-29 14:54:46 -0400

    blah

--- view/dashboard/left.php
+++ view/dashboard/left.php
@@ -1,7 +1,7 @@
 <div class="leftSideContainer">
 	<div class="leftSideMenu sticky-div">
 		<ul>
-			<li>Add Product</li>
+			<li><a href="#">Add Product</a></li>
 			<li><a href="#tab1" data-toggle="tab">Products</a></li>
 			<li><a href="#tab2" data-toggle="tab">Purchases</a></li>
 		</ul>
