9e2315d762e1457298fd8c7d913cc7d294728aa0
David <dzz0615@gmail.com>
2012-05-29 15:53:46 -0400

    incremental UI nav bar changes

--- view/dashboard/left.php
+++ view/dashboard/left.php
@@ -1,9 +1,13 @@
 <div class="leftSideContainer">
 	<div class="leftSideMenu sticky-div">
 		<ul>
+			<hr>
 			<li><a href="#">Add Product</a></li>
+			<hr>
 			<li><a href="#tab1" data-toggle="tab">Products</a></li>
+			<hr>
 			<li><a href="#tab2" data-toggle="tab">Purchases</a></li>
+			<hr>
 		</ul>
 	</div>
 </div>
