5c049555b00a3026f35dbae1706ca1fa037c3f96
Arianna Simpson <ariannahsimpson@gmail.com>
2012-05-30 20:22:13 -0400

    navbar
    git commit -am navbar
    git pull

--- view/templates/nav.php
+++ view/templates/nav.php
@@ -15,7 +15,7 @@ if($uid >= 0)
 		<div class="menu-navbar-bitlet">
 			<? if($uid >= 0) { // already logged in ?>
 					<div  class="dropdown">
-							<a class="dropdown-toggle" data-toggle="dropdown" href="#" id="userEmail">
+							<a class="dropdown-toggle rounded-corners" data-toggle="dropdown" href="#" id="userEmail">
 								<? echo $name; ?> <b class="caret" id="caret" ></b>
 							</a>
 							<ul class="dropdown-menu" id="menu1">
