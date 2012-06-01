a379557ed461eddeed8dfafeb14ff7d02bc2e06e
David <dzz0615@gmail.com>
2012-06-01 14:33:30 -0400

    better error page

--- view/error/index.php
+++ view/error/index.php
@@ -1,3 +1,10 @@
-<div class="container">
-	<h2>Oh snap! An error happened!</h2>
+<div class="container well bitletDropShadow" style="text-align: center">
+	<h1>Oh snap! An error happened!</h1>
+	<br>
+	<a class="btn btn-primary btn-large" href="/">Go back to main page</a>
+
+	<hr>
+	<h3>Here's some money to cheer you up.</h3>
+	<br>
+	<a href="/"><img style="width:200px;" src="/img/envelope.png"/></a>
 </div>
