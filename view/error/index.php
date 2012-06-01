d385d9c9cfb0e550f9572cc900e17e11b1e110da
David <dzz0615@gmail.com>
2012-06-01 14:43:14 -0400

    even better error page

--- view/error/index.php
+++ view/error/index.php
@@ -7,4 +7,13 @@
 	<h3>Here's some money to cheer you up.</h3>
 	<br>
 	<a href="/"><img style="width:200px;" src="/img/envelope.png"/></a>
+	<br>
+	<br>
+	<h3>Oh and some cats..</h3>
+	<a href="http://www.randomkittengenerator.com/"><img style="width:200px;" class="bitletDropShadow" src="http://placekitten.com/200/170"/></a>
+
+	<hr>
+	<h4>Feel better now?</h3>
+	<br>
+	<a class="btn btn-success btn-large" href="/">Yes!</a>
 </div>
