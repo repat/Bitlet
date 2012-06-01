58d17ac442cc5ee9b4d0ce4ad880391ae71f6d0f
David <dzz0615@gmail.com>
2012-05-31 21:03:22 -0400

    icons added

--- view/dashboard/left.php
+++ view/dashboard/left.php
@@ -3,11 +3,11 @@
 		<ul>
 			<li id="topCorner"></li>
 			<hr>
-			<li><a href="#"><img src="http://placekitten.com/100/100"/>Upload</a></li>
+			<li><a href="#"><img src="/img/upload.png"/>Upload</a></li>
 			<hr>
-			<li class="active"><a href="#tab1" data-toggle="tab"><img src="http://placekitten.com/100/100"/>Products</a></li>
+			<li class="active"><a href="#tab1" data-toggle="tab"><img src="/img/products.png"/>Products</a></li>
 			<hr>
-			<li><a href="#tab2" data-toggle="tab"><img src="http://placekitten.com/100/100"/>Purchases</a></li>
+			<li><a href="#tab2" data-toggle="tab"><img src="/img/purchases.png"/>Purchases</a></li>
 			<hr>
 		</ul>
 	</div>
