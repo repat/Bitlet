88650c3f0c9cf48faf44fdfbba7eee85d95ca3f4
David <dzz0615@gmail.com>
2012-06-01 18:14:27 -0400

    moved export file

--- bin/localhost_connect.sh
+++ bin/localhost_connect.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-/Applications/MAMP/Library/bin/mysql --host=localhost -uroot -proot < "lib/export.sql"
+/Applications/MAMP/Library/bin/mysql --host=localhost -uroot -proot < "bin/export.sql"
 
