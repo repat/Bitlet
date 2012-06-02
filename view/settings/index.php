adec4b40ebbca326587dbfa6204a973d49630d79
Arianna Simpson <ariannahsimpson@gmail.com>
2012-06-02 10:40:09 -0400

    preferences in settings

--- view/settings/index.php
+++ view/settings/index.php
@@ -2,12 +2,12 @@
 	<h1>Settings<h1>
 	<hr>
 	<h2>Profile</h2>
-		<div class="profile bitletDropShadow roundedCorners row" id="leftCol"> 
+		<div class="profile bitletDropShadow roundedCorners" id="leftCol"> 
 				<img id="profilePic" src="/img/team/david.png">
 				<h4 id="name">Name: <input class="input-small focused" id="focusedInput" type="text" value="David Zhang"></h4>
 				<h4 id="description">Description: <input class="input focused" id="focusedInput" type="text" value="Hacker, Cat Lover."></h4>
 				<input class="input-small focused" id="focusedInput" type="text" value="Princeton, NJ">
-				<button class="btn btn-small" id="uploadButton">Upload Image</button> 
+				<button class="btn btn-small" id="uploadButton-up">Upload Image</button> 
 	
 
 					<h4 id="website">Website: <input class="input-xlarge focused" id="focusedInput" type="text" value="www.davidzhang.com"></h4>
@@ -16,8 +16,8 @@
 		</div>
 	<hr>
 	<h2>Preferences</h2>
-		<div class="profile bitletDropShadow roundedCorners"> 
-			<p class="pref" ><input type="checkbox" id="check1" >Email me when someone purchases one of my products!</p>
+		<div class="profile bitletDropShadow roundedCorners" id="preferences"> 
+			<p class="pref" ></p><input type="checkbox" id="check1" >Email me when someone purchases one of my products!</p>
 			<p class="pref"><input type="checkbox" id="check2">Email me a weekly summary of my sales</p>
 			<h4 id="changePw">Change Password</h4>
 			<p class="pwField">Old Password:<input class="input focused" id="focusedInput-old" type="text" value="ILoveCats"></p>
