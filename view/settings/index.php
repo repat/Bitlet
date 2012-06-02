5effffcf1d05079a9e012e02711e54efb2eac809
Arianna Simpson <ariannahsimpson@gmail.com>
2012-06-02 00:41:54 -0400

    settings

--- view/settings/index.php
+++ view/settings/index.php
@@ -3,8 +3,13 @@
 	<hr>
 	<h2>Profile</h2>
 		<div class="profile bitletDropShadow roundedCorners"> 
-			<h3>David Zhang</h3>
+			<h4>Name: <input class="input-small focused" id="focusedInput" type="text" value="David Zhang"></h4>
+			<h4>Description: <input class="input-xlarge focused" id="focusedInput" type="text" value="Hacker, Cat Lover."></h4>
+			<input class="input-small focused" id="focusedInput" type="text" value="Princeton, NJ">
+			<h4>Website: <input class="input-xlarge focused" id="focusedInput" type="text" value="www.davidzhang.com"></h4>
+			<h4>Email: <input class="input-xlarge focused" id="focusedInput" type="text" value="david@ilovecats.com"></h4>
 			<button class="btn btn-small">Upload Image</button> 
+			
 			<button class="btn-primary">Save</button> 
 		</div>
 	<hr>
@@ -15,9 +20,9 @@
 			<input type="checkbox">
 			<p>Email me a weekly summary of my sales</p>
 			<h4>Change Password</h4>
-			<p>Old Password:</p>
-			<p>New Password:</p>
-			<p>Confirm New Password:</p>
+			<p>Old Password:</p><input class="input focused" id="focusedInput" type="text" value="ILoveCats">
+			<p>New Password:</p><input class="input focused" id="focusedInput" type="text" value="">
+			<p>Confirm New Password:</p><input class="input focused" id="focusedInput" type="text" value="">
 		
 		</div>
 </div>
