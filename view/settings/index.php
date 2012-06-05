32edf20bcbb071cfd064266c60ce6a3929757d7b
Arianna Simpson <ariannahsimpson@gmail.com>
2012-06-05 13:04:20 -0400

    settings adjustment

--- view/settings/index.php
+++ view/settings/index.php
@@ -1,8 +1,7 @@
-<div class="container">
-	<h1>Settings<h1>
-	<hr>
-	<h2>Profile</h2>
-		<div class="well container profile bitletDropShadow roundedCorners" id="leftCol"> 
+
+
+		<div class=" container well profile bitletDropShadow roundedCorners" id="leftCol"> 
+				<h2>Profile</h2>
 				<img id="profilePic" src="/img/team/david.png">
 				<div class="span4">
 				<h4 id="name">Name:</h4> <input class="input-small focused" id="focusedInput" type="text" value="David Zhang">
@@ -17,20 +16,18 @@
 					<br>
 					<input readonly type=text name=remLen id="textCount" size=3 maxlength=3 value="125">characters left</font>
 					</form>
-				
-					
 					<button class="btn btn-primary" id="save">Save</button>
 				</div>
 		</div>
-	<hr>
-	<h2>Preferences</h2>
-		<div class="well container profile bitletDropShadow roundedCorners" id="preferences"> 
+	
+	<div class="well container profile bitletDropShadow roundedCorners" id="preferences"> 
+			<h2>Preferences</h2>
 			<p class="pref" ><input type="checkbox" id="check1" >Email me when someone purchases one of my products!</p>
 			<p class="pref"><input type="checkbox" id="check2">Email me a weekly summary of my sales</p>
 			<h4 id="changePw">Change Password</h4>
-			<p class="pwField" id="old">Old Password:<input class="input focused" id="focusedInput-old" type="password" value="ILoveCats"></p>
+			<p class="pwField" id="old">Old Password:<input class="input focused" id="focusedInput-old" type="password" value=""></p>
 			<p class="pwField" id ="new">New Password:<input class="input focused" id="focusedInput" type="password" value=""></p>
 			<p class="pwField">Confirm New Password:<input class="input focused" id="focusedInput" type="password" value=""></p>
 			<img id="toolbox" src="/img/drawings/backend.png">
 		</div>
-</div>
+
