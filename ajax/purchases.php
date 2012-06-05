27939e1f35cfdb8c5d421a106db845c12eb96494
David <dzz0615@gmail.com>
2012-06-04 22:45:04 -0400

    ajaximg some stuff

---
+++ ajax/purchases.php
@@ -0,0 +1,16 @@
+<?
+
+$fids = GetPurchases($UID);
+
+$ret = '';	// return variable, start with empty string
+foreach($fids as $f) {
+	$finfo = GetFileInfo($f);
+
+	// build product row with file and add to return array
+	$ret .= BuildPurchaseRow($finfo['name'], $finfo['file_name'], $finfo['thumb_url']);
+}
+
+// return via AJAX
+echo $ret;
+
+?>
