bd05e7d715ea566785b9f79e19f4bce43f848764
Zachary Zimbler <zachzimbler@gmail.com>
2012-06-05 13:28:49 -0400

    changed a few files names in the templates

--- view/reset-details/index.php
+++ view/reset-details/index.php
@@ -1,3 +1,9 @@
 <div class="container well bitletDropShadow">
-
+	<form class="form-inline"> 
+        <label>Enter your email to reset your Bitlet password:</label>
+			<br />
+			<input type="text" class="span3" placeholder="Enter Your Email">
+        </label>
+        <button type="submit" class="btn">Reset Password</button>
+    </form>
 </div>
