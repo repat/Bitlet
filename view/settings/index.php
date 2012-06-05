f0463fcb3d614466b6bf31b3ddff1fe843bfc6ab
Arianna Simpson <ariannahsimpson@gmail.com>
2012-06-05 14:24:30 -0400

    settings

--- view/settings/index.php
+++ view/settings/index.php
@@ -1,5 +1,5 @@
 <div class=" container well profile bitletDropShadow roundedCorners" id="leftCol"> 
-	<h2>Profile</h2>
+	<h2 id="prof" >Profile</h2>
 		<img id="profilePic" src="/img/team/david.png">
 		<div class="span4">
 			<h4 id="name">Name:</h4> <input class="input-small focused" id="focusedInput" type="text" value="David Zhang">
@@ -14,18 +14,22 @@
 			<br>
 			<input readonly type=text name=remLen id="textCount" size=3 maxlength=3 value="125">characters left</font>
 			</form>
-			<button class="btn btn-primary btn-large" id="save">Save</button>
 		</div>
 		<br>
-		<div class="span11">
-			<hr>
-			<h2>Preferences</h2>
+		
+			<div class="span6 rightSide">
+				<hr>
+			<h2 >Preferences</h2>
 					<p class="pref" ><input type="checkbox" id="check1" >Email me when someone purchases one of my products!</p>
 					<p class="pref"><input type="checkbox" id="check2">Email me a weekly summary of my sales</p>
 					<h4 id="changePw">Change Password</h4>
 						<p class="pwField" id="old">Old Password:<input class="input focused" id="focusedInput-old" type="password" value=""></p>
 						<p class="pwField" id ="new">New Password:<input class="input focused" id="focusedInput" type="password" value=""></p>
 						<p class="pwField">Confirm New Password:<input class="input focused" id="focusedInput" type="password" value=""></p>
-		</div>
+				
+			</div>
+			<div class="span4">
 			<img id="toolbox" src="/img/drawings/backend.png">
+			<button class="btn btn-primary btn-large" id="save">Save</button>
+			</div>
 </div>
