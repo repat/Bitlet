126c1d4b1cfd6619f094995b3b8939b32e21b13d
Zachary Zimbler <zachzimbler@gmail.com>
2012-06-07 00:48:00 -0400

    nto sure whats going on

--- ajax/resetPassword.php
+++ ajax/resetPassword.php
@@ -1,7 +1,8 @@
 <?
-$email = $_POST['email'];
+	$email = $_POST['email'];
 
 	$uid = CheckUserEmail($email);
+	$name = GetUserName($uid);	
 	//check if the persons email validated and then reset or return error
 	if($uid === false) {
 		echo json_encode(array("success"=>false));
@@ -10,7 +11,7 @@ $email = $_POST['email'];
 		$pass = substr(md5(rand()), 0, 8); 
 		//setting the users password
 		SetUserPassword($uid, $pass);
+		EmailPasswordReset($email, $pass, $name);
 		echo json_encode(array("success"=>true));
 	}
-
 ?>
