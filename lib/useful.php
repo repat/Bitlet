d1431dc6433a6baa473f133b8624c2eb748d602c
David <dzz0615@gmail.com>
2012-06-13 03:32:26 -0400

    added useful lib file

---
+++ lib/useful.php
@@ -0,0 +1,14 @@
+<?
+
+function ReverseParseStr($args) 
+{
+	$str = ''; 
+	foreach ($args as $key => $value) 
+	{ 
+		$str .= (strlen($str) < 1) ? "?" : "&"; 
+		$str .= $key . '=' . rawurlencode($value); 
+	}
+	return $str;
+}
+
+?>
