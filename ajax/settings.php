34ca9f01ff8e108daed124de909fc87b069ef43c
Zachary Zimbler <zachzimbler@gmail.com>
2012-06-15 01:37:03 -0400

    added settings ajax file

---
+++ ajax/settings.php
@@ -0,0 +1,27 @@
+<?
+	$email = $_POST['email'];
+	$name = $_POST['name'];
+	$phone = $_POST['phone'];
+	$website = $_POST['website'];
+	$bio = $_POST['bio'];
+	$emailPurchase = $_POST['emailPurchase'];
+	$emailWeekly = $_POST['emailWeekly'];
+
+	$emailSettingsArray = array(
+		"EmailOnPurchase" => "$emailPurchase",
+		"EmailOnWeekly" => "$emailWeekly",
+	);
+	$emailSettings = ReverseParseStr($emailSettingsArray);
+	$uid = CheckUserEmail($email);
+
+	
+	//check if the persons email validated then update account 
+	error_log('the uid is '.$uid);
+	if($uid == null) {
+		error_log('Returning False');
+		echo json_encode(array("success"=>false));
+	} else {
+		UpdateUserSettings($uid, $email, $name, $phone, $website, $bio, $emailSettings);	
+		echo json_encode(array("success"=>true));
+	}
+?>
