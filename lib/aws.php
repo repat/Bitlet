0b5621718c7745518b9bd34b1a3a05e15170abb7
David <dzz0615@gmail.com>
2012-06-17 17:28:42 -0400

    beta AWS setup

--- lib/aws.php
+++ lib/aws.php
@@ -3,10 +3,19 @@
 require_once 'aws_lib/sdk.class.php';
 
 const BUCKET = 'bitlet_files';
+const BETA_BUCKET = 'bitlet_files_beta';
 
 // upload a file with fid currently stored in $location
 function AwsUpload($fid, $location)
 {
+	global $SERVER_CONFIG;
+
+	if($SERVER_CONFIG == 'server') {
+		$bucket = BUCKET;
+	} else {
+		$bucket = BETA_BUCKET;
+	}
+
 	// Instantiate the AmazonS3 class
 	$s3 = new AmazonS3();
 
@@ -15,7 +24,7 @@ function AwsUpload($fid, $location)
 
 	// Prepare to upload the file to our new S3 bucket. Add this
 	// request to a queue that we won't execute quite yet.
-	$file_upload_response = $s3->create_object(BUCKET, $name, array(	// we use the fid as file name
+	$file_upload_response = $s3->create_object($bucket, $name, array(	// we use the fid as file name
 		'fileUpload' => $location
 	));
 
@@ -30,6 +39,14 @@ function AwsUpload($fid, $location)
 // get url of file based on fid
 function AwsGetUrl($fid)
 {
+	global $SERVER_CONFIG;
+
+	if($SERVER_CONFIG == 'server') {
+		$bucket = BUCKET;
+	} else {
+		$bucket = BETA_BUCKET;
+	}
+
 	// Instantiate the AmazonS3 class
 	$s3 = new AmazonS3();
 
@@ -37,8 +54,8 @@ function AwsGetUrl($fid)
 	/* Display a URL for each of the files we uploaded. Since uploads default to
 	   private (you can choose to override this setting when uploading), we'll
 	   pre-authenticate the file URL for the next 5 minutes. */
-	$url = $s3->get_object_url(BUCKET, $name, '5 minutes');
-	$size = $s3->get_object_filesize(BUCKET, $name);
+	$url = $s3->get_object_url($bucket, $name, '5 minutes');
+	$size = $s3->get_object_filesize($bucket, $name);
 	return array($url, $size);
 }
 
