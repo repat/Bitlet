889b7d45caf28861670a677984cc8446a22f3171
David <dzz0615@gmail.com>
2012-06-18 12:02:16 -0400

    fixed bug

--- view/templates/useful.js
+++ view/templates/useful.js
@@ -2,7 +2,7 @@
 function EmailValid(email) {
 	// emails should be at least 5 characters, and contain a '@' and a '.'
 	var aix = email.indexOf('@');
-	var dix = email.lastindexOf('.');
+	var dix = email.lastIndexOf('.');
 	return (email.length > 4 && aix != -1 && dix != -1 && aix < dix);
 };
 
