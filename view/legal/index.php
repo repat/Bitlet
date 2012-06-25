e7490f2bb7cbd4d4303a9727570b52ff9986813e
Arianna Simpson <ariannahsimpson@gmail.com>
2012-06-25 13:25:48 -0400

    legal

--- view/legal/index.php
+++ view/legal/index.php
@@ -1,24 +1,95 @@
 <div class="container faq">
 	<div class="row">
-		<div class="span8">	
+		<div class="span12">	
 			<div class="well">	
-				<h1>Terms of Use</h1>
+				<h1>Terms of Use</h1></br>
+					<h3>1.	Acknowledgement, Understanding, and Acceptance of Agreement </h3></br>
+							<p>The following Agreement is an electronic contract that details the legally binding terms of use of the Bitlet Website and Service. Please read this Agreement carefully; if you do not agree with the terms of use, do not use the Website or Service. Bitlet reserves the right to modify this agreement at any time without previously notifying users; as such, the burden of periodically reviewing these terms falls with the user. By selling or purchasing Digital Goods or by downloading or using the Bitlet Link, you accept and agree to be bound by the terms of this Agreement, whether or not you register with Bitlet. We strongly recommend that, as you read this User Agreement, you also access and read any linked information. This Agreement includes Bitlet’s Privacy Policy, and any notices regarding the Website or the Service. You, the User, agree and understand that certain features of the Website or the Service may be subject to additional terms and conditions or registration requirements. You agree to abide by these additional terms and you further agree that a violation of those terms while you are using the Service or accessing the Website shall constitute a breach of this Agreement. Use of the Service or the Website is permitted only by individuals who can form legally binding contracts under applicable law. Without limiting the foregoing, use of the Service or Website is not permitted by anyone under the age of 18. Please refrain from using the Service or the Website if you are under the age of 18. </p>
+					<h3>2.	Terms of Use for Bitlet </h3></br>
+							<p>If you elect to use Bitlet, including the Website, Service, links and all related components, you agree to refrain from any and all of the following: violating any laws, agreements, policies, third party rights; using Bitlet and all associated links, Services, Websites etc if you are under the age of 18, are unable to form legally binding contracts, or have been suspended from using Bitlet; failing to provide payment for any good you have purchased; failing to deliver any goods which have been purchased from you; providing misleading, false, or libelous information about other users, Bitlet and the goods which you are selling,; posting or sharing spam, viruses,  chain letters, or unauthorized promotional materials of any sort; copying all or parts of the Bitlet Website,  Service, content, trademarks, copyrights etc.; gathering information about Users without their explicit written permission, including but not limited to: email, name, address, demographic data, and purchase history; stealing from or otherwise manipulating Bitlet Services, payment structures, billing system, or fees owed to Bitet or other users; using Bitlet with malicious or dishonest intent.</p>
+					<h3>3.	Misuse of the Website and Service </h3></br>
+							<p>We reserve the right to place temporary or permanent restrictions on a User’s access to the Website, Service, and all affiliated services, tools and content if the User is abusing Bitlet. At our sole discretion, we may suspend your account(s) or hold any funds in your Bitlet account(s) if we have cause to suspect that you have been involved in abuse of the Website and Service. Abuse of Bitlet includes, but is not limited to, uploading copyrighted content without ownership or possessing the intellectual property rights to said content; using Bitlet to commit fraud or otherwise harm or violate the rights of members of the community of Users, affiliates, or Bitlet employees; spreading unsolicited commercial or advertising messages through the Bitlet Website or Service; breaching or failing to comply with any portion of this Agreement. We also reserve the right to review and remove uploaded content.</p>
+					<h3>4.	Payments and Fees </h3></br>
+							<p>Sellers on Bitlet are charged a flat fee of 30 cents per transaction, plus 7% of the sale price.  Bitlet reserves the right to modify this pricing structure at any time, so Users are advised to review the pricing structure periodically to ensure that they are aware of any changes. Upon electronic signature of this Agreement, Sellers also agree to pay any and all related taxes and fees associated with selling their product.  Profits earned from products sold on Bitlet will be disbursed in a previously agreed upon manner (PayPal, check, etc.) within a time window of 90 days from the date payment was received from the Buyer.  Payments will be disbursed when the total reaches a minimum sum of $10.
+							<p>	Buyers may use the Website and Service free of charge.</p>
+					<h3>5.	Content</h3> </br>
+							<p>Bitlet does not accept or claim any ownership of, or rights to, products or goods uploaded and listed on/through the Website or Service. You, the User, maintain all control, ownership, and responsibility of and for your products. 
+							This applies to all data, links, files and all other content listed for sale by sellers. All information provided to Bitlet and other Users must be correct, true, and up-to-date. 
+							Bitlet maintains the right to determine what is considered “suitable content” for sale on the Website and Service, and to modify any this definition at any time without previously notifying Users, and to remove any and all content that fails to comply with this definition. 
+							You agree to refrain from posting, sharing, communicating, or otherwise spreading content that, in full or in part, contains anything illegal, obscene, defamatory, libelous, profane, inaccurate, pornographic, child pornographic, morally reprehensible, hateful, harmful to children and minors; contains viruses, spam, bots, or any similar harmful device; contains copyrighted materials of any nature unless explicit written permission has previously been obtained; contains the likeness, image, data or personal information of another person unless explicit written permission has previously been obtained; encourages involvement in a political party, religious movement or sect; impersonates or creates a false association with a third party; promotes or enables gambling; promotes discrimination against any gender, race, or other group.</p> 
+					<h3>6.	Conditions for Uploading</h3></br>
+							<p>Bitlet reserves the right to hold products until it has reviewed them and deemed them suitable for sale on the Website. You also agree that Bitlet may remove the listing for any product at any time at its sole discretion with no penalty, without any obligation to notify you or supply an explanation for the removal. When you upload and list a product on Bitlet, you agree that you are fully responsible for the contents of the product. You agree that your product is not in violation of any law in your country or any United States federal or state law. </p>
+					<h3>7.	Privacy </h3></br>
+							<p>Bitlet will not lease or sell your personal information to third parties without your explicit written consent. For more information, please review Bitlet’s Privacy Policy. In signing this Agreement, you also agree to the Bitlet Privacy Policy.</p>
+					<h3>8.	Indemnification </h3></br>
+							<p>You agree to indemnify, defend and hold Bitlet and all employees, affiliates, licensors, and business partners harmless from any claim, demand, damages or other losses, including but not exclusive to reasonable attorneys' fees, asserted by any third-party resulting from or arising out of any breach by you of this Agreement or any other policy established by Bitlet. </p>
+					<h3>9.	Disputes</h3></br>
+							<ol class="legal">
+								<li><h4>Disputes between you and Bitlet </h4>
+									<p>Should a dispute arise between you and Bitlet, we hope to resolve the issue quickly and in a cost-effective manner. We encourage you to contact us at team@bitlet.co prior to taking legal action. If you and Bitlet are unable to reach a resolution in this manner, you and Bitlet agree to resolve any claim or controversy at law or equity in accordance with this agreement. Bitlet will consider resolving disputes in a reasonable manner through alternative methods, such as arbitration or mediation, in lieu of litigation. </p>
+									<p>If believe your copyrighted work has been utilized in a way that infringes on the copyright, is in violation of the Digital Millennium Copyright Act of 1998 (DMCA), and is accessible through our Service our Website, please contact in writing. It is Bitlet’s policy to be fully in compliance with the DCMA. </p></li>
+								<li><h4>Disputes between Users </h4>
+									<p>If you have a dispute with another User, you agree to release Bitlet and all employees, affiliates, licensors, and business partners from any claim, damage, or demand of every nature arising from use of the Website or Service or in any way related to the dispute. </p>
+							</ol>
+					<h3>10.	 Digital Millenium Copyright Acts Notice</h3></br>
+							<ol class="legal">
+									<li>a.	If you believe your copyrighted work has been utilized in a way that infringes on the copyright, is in violation of the Digital Millennium Copyright Act of 1998 (DMCA), and is accessible through our Service our Website, please contact us in writing. It is Bitlet's policy to be fully in compliance with the DCMA.</li>
+									<li>b.	In order for your complaint to be valid under the DCMA, you must contact us in writing with ALL of the following information: </li>
+										<ol class="legal">
+											<li>i.	At the top of your notice, the words "DMCA COMPLAINT - TAKEDOWN NOTICE";</li>
+											<li>ii.	A detailed description of the copyrighted work(s) or other intellectual property(s) you claim has/have been violated;</li>
+											<li>iii.	Identification of the specific Client Material that you believe infringes upon your rights and sufficient information to enable Bitlet to locate the specific file or material.  Per the DMCA, Bitlet is only authorized to remove or disable access to specific infringing files or materials; </li>
+											<li>iv.	The following statements or equivalent:
+												<ol class="legal"> <li>"I swear, under penalty of perjury, that</li>
+													<ol class="legal"> <li> the Information contained in this notification is accurate and that I am the copyright owner or am authorized to act on behalf of the owner of an exclusive right that is allegedly infringed; and </br>
+														<li>I have a good faith belief that use of the copyrighted materials described above on the allegedly infringing web pages is not authorized by the copyright owner, its agent, or the law."</br>
+													</ol>
+												</ol>	
+											<li>v.	Your company name (if any), your legal name (printed), your mailing address, and your phone number, and, if available, an electronic email address; and </li>
+											<li>vi.	A physical or electronic signature of a person authorized to act on behalf of the owner of the right that is allegedly violated or infringed, and the present date.	</li>
+										</ol>
+									<li>c.	To file a DMCA complaint, you must send a communication via certified-mail to Bitlet’s designated representative at the following address or email address:</br>
+										<ol class="legal">
+											<li>Bitlet, Inc. </li>
+											<li>39 Wiggins Street, Apt. 5</li>
+											<li>Princeton, NJ 08540 </li>
+											<li>Email: team@bitlet.co</li>
+										</ol>	
+									<li>d.	Upon receiving your complaint, Bitlet may remove the materials in question. Bitlet furthermore may elect to terminate or suspend the account of the User(s) who appear(s) to be violating your intellectual property rights.</li>
+									<li>e.	Note:  Submitting a false or materially misleading Takedown Notice may constitute perjury and other offenses and may subject you to potential liabilities. </li>
+							</ol>
+					<h3>11.	Limitation of Liability</h3></br>
+						<p>IN NO EVENT AND UNDER NO CIRCUMSTANCES WILL BITLET OR ANY EMPLOYEE, AFFILIATE, LICENSOR, OR BUSINESS PARTNER BE LIABLE FOR ANY DAMAGES, LOSSES AND/OR LIABILITIES INCLUDING THOSE BASED IN DIRECT, INDIRECT, INCIDENTAL, CONSEQUENTIAL OR SPECIAL DAMAGES IN ANY WAY RELATED TO THE USE OF THE WEBSITE, SERVICE, OR ANY RELATED GOODS OR CONTENT.BITLET IS NOT LIABLE FOR LOSS OF PROFITS OR ANY OTHER DAMAGE ARISING FROM THE USER’S USE, MISUSE, OR INABILITY TO USE THE WEBSITE OR SERVICE. IN AGREEING TO THESE TERMS, THE USER ACCEPTS ALL RISK AND RESPONSIBILITY ASSOCIATED WITH BITLET. BITLET EXCLUDES ALL IMPLIED WARRANTIES, TERMS AND CONDITIONS, INCLUDING BUT NOT LIMITED TO THOSE OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND NON-INFRINGEMENT. 
+							WITHOUT LIMITING THE PREVIOUS PARAGRAPH, UNDER NO CASE AND IN NO CIRCUMSTANCES SHALL THE TOTAL LIABILITY OF BITLET AND AFFILIATES OR RELATED PARTIES EXCEED THE AMOUNT PAID BY THE USER TO BITLET IN THE THREE (3) MONTHS PRIOR TO THE ACTION CAUSING THE LIABILITY. 
+							
+							BITLET DECLINES ANY RESPONSIBILITY FOR CONTENT UPLOADED OR LISTED BY OTHER USERS, AND IS NOT INVOLVED IN THE TRANSACTION BETWEEN THE BUYER AND SELLER. AS SUCH, YOU AGREE THAT BITLET DOES NOT TRANSFER LEGAL OWNERSHIP OF ANY PRODUCT OR ITEM FROM THE SELLER TO THE BUYER. WHILE BITLET FACILITATES THE TRANSFER OF DIGITAL PRODUCTS BETWEEN THE SELLER AND THE BUYER, ALL RESPONSIBILITIES FOR THE SALE REST WITH THE SELLER AND BUYER. 
+							
+							Due to the fact that states or jurisdictions do not allow the exclusion or the limitation of liability for consequential or incidental damages, in such states or jurisdictions, the liability of Bitlet and its affiliates shall be limited to the fullest extent permitted by law. </p>
+					<h3>12.	General </h3></br>
+						<ol class="legal">
+							<h4>a. No partnership</h4>
+								<ol class="legal"><li>i.You agree that no partnership, employment, joint venture, or agency relationship exists between you and Bitlet as a result of this Agreement or your use of the Website and/or Service. You are not authorized to make any decisions, promises, or warranties on behalf of Bitlet. You agree that you will not present yourself as an agent or representative of Bitlet.</li></ol>
+							<h4>b.	Severability</h4>
+								<ol class="legal"><li>i.In the event that any portion of this Agreement is found in any court to be invalid or unenforceable, the remainder of the Agreement will not be affected and shall be enforced. </li></ol>
+							<h4>c.	Notices</h4>
+								<ol class="legal"><li>i. All notices shall be sent in writing to the following address:</li> </br>
+									<ol class="legal"><li>Bitlet, Inc.</li></ol>
+									<ol class="legal"><li>39 Wiggins Street, Apt. 5</li></ol>
+									<ol class="legal"><li>Princeton, NJ 08540</li></ol>
+						</ol>	
+						<ol class="legal">
+							<li>ii.Bitlet will contact a User at the address and/or e-mail address provided at the time s/he uploaded a product or created an account. </li>
+						</ol>
+						<h4>d.Headings </h4>
+							<ol class="legal">
+								<li>i. The headings included in this Agreement are provided for your convenience only and in no way limit or affect the scope, meaning, or validity of any part of the Agreement. </li></br> </p>
+							</ol>
+						</ol>
 			</div>
 		</div>
 		<div class="span4">
-			<div class="well" style="padding: 8px 0;position:fixed;width:300px;">
-				<ul class="about-nav nav nav-list">
-					<li class="nav-header"><a href="">Terms</a></li>
-					<li><a href="#">1.Home</a></li>
-					<li><a href="#">2.Library</a></li>
-					<li><a href="#">3.Applications</a></li>
-					<li class="nav-header"><a href="">Privacy</a></li>
-					<li><a href="#">1.Profile</a></li>
-					<li><a href="#">2.Settings</a></li>
-					<li><a href="#">3.Help</a></li>
-					<li class="nav-header"><a href="">Security</a></li>
-				</ul>
-			</div>
+	
+
 		</div>
 
 	</div>
